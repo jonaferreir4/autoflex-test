b549b581c52d7ed3dc724e399880dcf8098a4491
jonaferreir4 <jonaferreira2204@gmail.com>
2026-02-04 23:18:07 -0300

    feat: implement Product entity with fields for id, name, and value

--- backend/src/main/java/com/autoflex/api/entity/Product.java
+++ backend/src/main/java/com/autoflex/api/entity/Product.java
@@ -1,4 +1,21 @@
 package com.autoflex.api.entity;
 
+import jakarta.persistence.*;
+import lombok.Data;
+import java.math.BigDecimal;
+
+@Data
+@Entity
+@Table(name = "products")
 public class Product {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(nullable = false)
+    private BigDecimal value;
 }
