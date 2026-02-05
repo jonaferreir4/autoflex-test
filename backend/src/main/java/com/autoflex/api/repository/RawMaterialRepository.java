b77bdc22642e3c45023925a21e413fe383733fbb
jonaferreir4 <jonaferreira2204@gmail.com>
2026-02-04 23:28:34 -0300

    feat: create RawMaterialRepository interface for managing RawMaterial entities

---
+++ backend/src/main/java/com/autoflex/api/repository/RawMaterialRepository.java
@@ -0,0 +1,9 @@
+package com.autoflex.api.repository;
+
+import com.autoflex.api.entity.RawMaterial;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface RawMaterialRepository extends JpaRepository<RawMaterial, Long> {
+}
