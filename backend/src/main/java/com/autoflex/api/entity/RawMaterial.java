49b417b2da65b2c87039b67fe946452a74345082
jonaferreir4 <jonaferreira2204@gmail.com>
2026-02-04 23:18:59 -0300

    feat: define RawMaterial entity with fields for id, name, and stock quantity

--- backend/src/main/java/com/autoflex/api/entity/RawMaterial.java
+++ backend/src/main/java/com/autoflex/api/entity/RawMaterial.java
@@ -1,4 +1,20 @@
 package com.autoflex.api.entity;
 
+import jakarta.persistence.*;
+import lombok.Data;
+
+@Data
+@Entity
+@Table(name = "raw_materials")
 public class RawMaterial {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String name;
+    
+    @Column(name = "stock_quantity", nullable = false)
+    private Integer stockQuantity;
 }
