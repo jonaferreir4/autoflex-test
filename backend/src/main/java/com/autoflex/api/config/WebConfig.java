bd8770674c4da81a0e21dcbb18a969338dcf905e
jonaferreir4 <jonaferreira2204@gmail.com>
2026-02-05 14:44:27 -0300

    feat: add CORS configuration for API endpoints

---
+++ backend/src/main/java/com/autoflex/api/config/WebConfig.java
@@ -0,0 +1,23 @@
+package com.autoflex.api.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**") // Aplica a todos os endpoints da API
+                .allowedOrigins(
+                        "http://localhost:5173",
+                        "http://localhost:3000",
+                        "http://localhost:80",   
+                        "http://localhost"      
+                )
+                .allowedMethods("GET", "POST", "PUT", "PACTH",  "DELETE", "OPTIONS", "HEAD", "TRACE", "CONNECT")
+                .allowedHeaders("*")
+                .allowCredentials(true);
+    }
+}
