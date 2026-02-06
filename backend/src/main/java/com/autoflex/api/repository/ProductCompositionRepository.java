9197a85382a2a1b0a050f412bb48495447ab3a98
jonaferreir4 <jonaferreira2204@gmail.com>
2026-02-05 23:27:36 -0300

    feat: add method to find ProductComposition by product and raw material IDs

--- backend/src/main/java/com/autoflex/api/repository/ProductCompositionRepository.java
+++ backend/src/main/java/com/autoflex/api/repository/ProductCompositionRepository.java
@@ -4,8 +4,11 @@ import com.autoflex.api.entity.ProductComposition;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import java.util.List;
+import java.util.Optional;
 
 @Repository
 public interface ProductCompositionRepository extends JpaRepository<ProductComposition, Long> {
     List<ProductComposition> findByProductId(Long productId);
+
+    Optional<ProductComposition> findByProductIdAndRawMaterialId(Long productId, Long rawMaterialId);
 }
